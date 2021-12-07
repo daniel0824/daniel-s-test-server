9eea5f3a9105278855a765bf8622b5bd37eee2f6
daniel0824 <95666244+daniel0824@users.noreply.github.com>
2021-12-06 18:58:47 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
