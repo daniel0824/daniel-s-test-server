f8a38aa3a636bae152528df2973f50d875098494
daniel0824 <95666244+daniel0824@users.noreply.github.com>
2021-12-06 19:17:21 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@ function windowResized(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
   background(220);
-  fill(255,0,0);
-  ellipse(mouseX, mouseY, 200, 200);
-  fill(0, 255,0)
-  ellipse(mouseX, mouseY, 100, 100);
+  for(let x = 0; x < width; x+=50){
+    for(let y = 0; y < height; y+= 50){
+      fill(dist(mouseX, mouseY, x, y)   );
+      ellipse(x, y, 50, 50);
+    }
+  }
 }
